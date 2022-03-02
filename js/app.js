77e97e73abbb77167e75d6b10024962059b58198
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-02 07:53:23 +0600

    Add search and get result feature

--- js/app.js
+++ js/app.js
@@ -3,11 +3,24 @@ const API_KEY = `42cfe03edeb792198487cb1f9c0bed72`;
 const searchWeather = () => {
     const searchLocationField = document.getElementById('searchLocation');
     const searchLocation = searchLocationField.value.toLowerCase();
+    searchLocationField.value = '';
     getDataFromApi(searchLocation);
 };
 const getDataFromApi = async (getLocation) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${getLocation}&appid=${API_KEY}&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    displayWeather(data);
+};
+
+const displayWeather = weather => {
+    console.log(weather);
+    setInnerText('city_name', weather.name);
+    setInnerText('city_current_temp', weather.main.temp);
+    setInnerText('weather_mode', weather.weather[0].main);
+};
+
+const setInnerText = (getId, value) => {
+    const element = document.getElementById(getId);
+    element.innerText = value;
 };
